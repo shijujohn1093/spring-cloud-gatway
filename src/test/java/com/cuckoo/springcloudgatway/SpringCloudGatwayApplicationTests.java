43f8546726c4667d77dea97d613a0d42cead090f
shijujohn1093 <shijujohn1093@gmail.com>
2022-07-05 23:23:27 +0530

    first commit

---
+++ src/test/java/com/cuckoo/springcloudgatway/SpringCloudGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuckoo.springcloudgatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
